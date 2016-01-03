8e5d09d1c0cca85b651f972528d7a75dda25002e
Vaillant Samuel <samuel.vaillant@captaindash.com>
2016-01-03 19:03:36 +0100

    feat: add default and start task

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,17 @@
 'use strict';
 
-require('require-dir')('./tasks');
+const gulp = require('gulp');
+const HubRegistry = require('gulp-hub');
+
+new HubRegistry(['./tasks/*.js']);
+
+gulp.task('default', gulp.series(
+  'clean',
+  gulp.parallel('transpile', 'copy')
+));
+
+gulp.task('start', gulp.series(
+  'default',
+  'server',
+  'watch'
+));
