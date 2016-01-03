43bddf60f27c4400bbab0e728b7051feffaf61e1
Vaillant Samuel <samuel.vaillant@captaindash.com>
2016-01-03 19:02:12 +0100

    fix(transpile): avoid to get the server at the top

--- tasks/transpile.js
+++ tasks/transpile.js
@@ -3,7 +3,7 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const rename = require('gulp-rename');
-const bs = require('browser-sync').get('server');
+const bs = require('browser-sync');
 
 // Create TS project for incremental build and load TS configuration
 const tsProject = ts.createProject('./tsconfig.json');
@@ -13,7 +13,7 @@ function transpileTS() {
     .pipe(ts(tsProject))
     .pipe(rename({ dirname: '' }))
     .pipe(gulp.dest('./dist'))
-    .pipe(bs.stream())
+    .pipe(bs.get('server').stream())
 }
 
 gulp.task('transpile:ts', transpileTS);
