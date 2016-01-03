c20ed42f0417ad1850f969fea7ffef83cbc40ef4
Vaillant Samuel <samuel.vaillant@captaindash.com>
2016-01-03 16:50:49 +0100

    refactor(copy): split in two task static and template

--- tasks/copy.js
+++ tasks/copy.js
@@ -4,14 +4,23 @@ const gulp = require('gulp');
 const Stream = require('stream');
 const mergeStream = require('merge-stream');
 
-const paths = {
-  './jspm.config.js': './dist',
-  './src/jspm_packages/**/*': './dist/jspm_packages',
-  './src/**/*.html': './dist',
-};
-
-gulp.task('copy', () => {
+function copy(paths) {
   return Object.keys(paths).reduce((acc, path) => {
     return mergeStream(acc, gulp.src(path).pipe(gulp.dest(paths[path])));
   }, gulp.src('.'));
+}
+
+gulp.task('copy:static', () => {
+  return copy({
+    './jspm.config.js': './dist',
+    './src/jspm_packages/**/*': './dist/jspm_packages',
+  });
 });
+
+gulp.task('copy:template', () => {
+  return copy({
+    './src/**/*.html': './dist',
+  });
+});
+
+gulp.task('copy', gulp.parallel('copy:static', 'copy:template'));
