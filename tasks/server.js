23bcec51c76cbb5a71fed2bb42edf377235a2ae3
Vaillant Samuel <samuel.vaillant@captaindash.com>
2016-01-03 16:51:13 +0100

    feat: add auto reload

--- tasks/server.js
+++ tasks/server.js
@@ -3,6 +3,11 @@
 const gulp = require('gulp');
 const bs = require('browser-sync').create('server');
 
+gulp.task('server:reload', done => {
+  bs.reload();
+  done();
+});
+
 gulp.task('server', () => {
   bs.init({
     server: {
--- tasks/transpile.js
+++ tasks/transpile.js
@@ -3,6 +3,7 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const rename = require('gulp-rename');
+const bs = require('browser-sync').get('server');
 
 // Create TS project for incremental build and load TS configuration
 const tsProject = ts.createProject('./tsconfig.json');
@@ -11,7 +12,8 @@ function transpileTS() {
   return tsProject.src()
     .pipe(ts(tsProject))
     .pipe(rename({ dirname: '' }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist'))
+    .pipe(bs.stream())
 }
 
 gulp.task('transpile:ts', transpileTS);
