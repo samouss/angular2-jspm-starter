33ca8dd4e7b47a1469f9eb6eef3409da60511ef8
Vaillant Samuel <samuel.vaillant@captaindash.com>
2016-01-04 21:15:45 +0100

    feat(starter): add binding and event

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { bootstrap } from 'angular2/platform/browser'
+import { bootstrap } from 'angular2/platform/browser';
 import StarterComponent from './starter/starter.component';
 
 bootstrap(StarterComponent);
--- src/starter/starter.component.ts
+++ src/starter/starter.component.ts
@@ -5,9 +5,18 @@ import { Component } from 'angular2/core';
     template: `
       <div class="container">
         <div class="jumbotron">
-          <h2>Angular 2 JSPM Starter</h2>
+          <h2>{{ name }}</h2>
+          <button (click)="log()" class="btn btn-info">Click and see the console !</button>
         </div>
       </div>
-    `
+    `,
 })
-export default class AppComponent { }
+export default class AppComponent {
+
+  name: string = 'Angular2 JSPM Starter';
+
+  log(): void {
+    console.log('Yeahh !');
+  }
+
+}
