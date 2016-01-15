d4e3c2630d238dbf73483c3bf62da9f272799ee4
Vaillant Samuel <samuel.vaillant@captaindash.com>
2016-01-15 12:07:10 +0100

    fix(watch): update glob path and add callback done

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
+const excludes = ['!./src/jspm_packages/**'];
 
-gulp.task('watch', () => {
+gulp.task('watch', (done) => {
 
-  gulp.watch('./src/**.ts', gulp.series('transpile:dist'));
+  gulp.watch(['./src/**/*.ts'].concat(excludes), gulp.series('transpile:dist'));
 
-  gulp.watch('./src/**.html', gulp.series('copy:dist:template', 'server:reload'));
+  gulp.watch(['./src/**/*.html'].concat(excludes), gulp.series('copy:dist:template', 'server:reload'));
+
+  done();
 
 });
 
 gulp.task('watch:test', (done) => {
 
-  gulp.watch('./src/**.ts', gulp.series('transpile:tmp'));
+  gulp.watch('./src/**/*.ts', gulp.series('transpile:tmp'));
 
   done();
 
