894d448d550fa02544aa68aebeeea71bba07fd80
Vaillant Samuel <samuel.vaillant@captaindash.com>
2016-01-16 18:59:33 +0100

    refactor: load bootstrap as module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// Import CSS via SystemJS plugin CSS
+import 'bootstrap/css/bootstrap.min.css!';
+
+// Import Angular 2 modules
 import { bootstrap } from 'angular2/platform/browser';
 import StarterComponent from './app/starter/starter.component';
 
