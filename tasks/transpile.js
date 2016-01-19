62de3fb09e8791f1491bbdb37bbaf02f35f92b90
Vaillant Samuel <samuel.vaillant@captaindash.com>
2016-01-19 14:49:58 +0100

    feat(TSLint): add support

--- tasks/transpile.js
+++ tasks/transpile.js
@@ -2,32 +2,52 @@
 
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
+const tslint = require('gulp-tslint');
 const plumber = require('gulp-plumber');
 const sourcemaps = require('gulp-sourcemaps');
 const notify = require('gulp-notify');
 const bs = require('browser-sync');
 const PATHS = require('./constant').PATHS;
 
+const files = [
+  "./src/**/*.ts",
+  "!./src/jspm_packages/**/*",
+];
 const config = require('../tsconfig.json');
 
 /**
  * @name   transpileTS
- * @desc   Tanspile TypeScript file and remove `src` base folder from directory
+ * @desc   Tanspile TypeScript file
  * @return {Stream}
  */
-function transpileTS(name, destPath) {
-  return gulp.src([
-      "./src/**/*.ts",
-      "!./src/jspm_packages/**/*",
-    ], { since: gulp.lastRun(name) })
-    .pipe(plumber({errorHandler: notify.onError("TS compilation failed !")}))
-    .pipe(sourcemaps.init())
-      .pipe(ts(config.compilerOptions))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(destPath))
-    .pipe(bs.get('server').stream());
+ function transpileTS(name, destPath) {
+   return function transpileTS() {
+     return gulp.src(files, { since: gulp.lastRun(name) })
+       .pipe(plumber({errorHandler: notify.onError("TS compilation failed !")}))
+       .pipe(sourcemaps.init())
+         .pipe(ts(config.compilerOptions))
+       .pipe(sourcemaps.write())
+       .pipe(gulp.dest(destPath))
+       .pipe(bs.get('server').stream());
+   }
+ }
+
+/**
+ * @name   lintTS
+ * @desc   Lint TypeScript file
+ * @return {Stream}
+ */
+function lintTS(name) {
+  return function lintTS() {
+    return gulp.src(files, { since: gulp.lastRun(name) })
+      .pipe(plumber({errorHandler: notify.onError("TS linting failed !")}))
+      .pipe(tslint())
+      .pipe(tslint.report('prose'));
+  };
 }
 
-gulp.task('transpile:dist', () => transpileTS('transpile:dist', PATHS.DIST_PATH));
+gulp.task('lintTS', lintTS('lintTS'));
 
-gulp.task('transpile:tmp', () => transpileTS('transpile:tmp', PATHS.TMP_PATH));
+gulp.task('transpile:dist', gulp.series('lintTS', transpileTS('transpile:dist', PATHS.DIST_PATH)));
+
+gulp.task('transpile:tmp', gulp.series('lintTS', transpileTS('transpile:tmp', PATHS.TMP_PATH)));
